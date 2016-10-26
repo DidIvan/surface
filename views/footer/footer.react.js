704a78f93c4b600fea7fc60dcbaeb3a29ab809c1
Ivan <Holy.vinil@gmail.com>
2016-10-26 22:51:56 +0300

    footer

--- views/footer/footer.react.js
+++ views/footer/footer.react.js
@@ -1,21 +1,17 @@
 var React = require("react");
 
 var Footer = React.createClass({
-    componentDidMount: function () {
-    },
     render: function () {
         return (
             <footer id="footer">
                 <div className="container container_footer">
-                    <div className="header-top">
-                        <a className="logo" href="#"><img src="../../img/logo.png" alt="logo"/></a>
-                        <p className="right_logo">for athletes and coaches</p>
-                    </div>
-                    <div className="row">
-                        <a href="#" className="button button_1"><img src="../../img/footer/fig1.png"
+                    <div className="row footer_top">
+                        <a href="#" className="button_download"><img src="../../img/footer/Artwork.png"
                                                                      alt="download"/></a>
-                        <p className="footer_title1">download app</p>
-                        <p className="footer_title2">show everyone what you can do!</p>
+                        <div className="footer_titles">
+                            <p className="footer_title1">download app</p>
+                            <p className="footer_title2">show everyone what you can do!</p>
+                        </div>
                     </div>
                     <div className="row footer_center buttons">
                         <a href="#" className="button button_1"><img src="../../img/header/IconApple.png"
@@ -35,7 +31,7 @@ var Footer = React.createClass({
                             <a href="#" className="button_link"><img src="../../img/footer/instagram.png"
                                                                      alt="instagram"/></a>
                         </div>
-                       
+
                         <p>Contact Us | Terms & Conditions</p>
                     </div>
                 </div>

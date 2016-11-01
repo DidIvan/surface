881377f41b1119ff38de4a07a96ff0d6c290748c
Ivan <Holy.vinil@gmail.com>
2016-11-01 20:57:58 +0200

    add a new component - players

--- views/main/content/players.react.js
+++ views/main/content/players.react.js
@@ -4,35 +4,34 @@ var Players = React.createClass({
     render: function () {
         return (
             <div>
-                <section className="athletes">
-                    <div className="athletes_row1">
-                        <div className="athletes_right">
+                <section className="players">
+                    <div className="players_row1">
+                        <div className="players_right">
                             <h2>Athletes</h2>
                             <div className="connection">
                                 <span>01</span>
                                 <h3>connection</h3>
-                                <p>Connect with coaches directly, you can ping coaches to view profile.</p>
+                                <p>Connect with talented athlete directly, you can watch their
+                                    skills through video showreels directly from Surface 1.</p>
                             </div>
                         </div>
                     </div>
-                    <div className="athletes_row2">
-                        <div className="athletes_right">
+                    <div className="players_row2">
+                        <div className="players_right">
                             <div className="collaboration">
                                 <span>02</span>
                                 <h3>collaboration</h3>
-                                <p>Work with other student athletes to increase visibility. When you share and like
-                                    other players videos it will increase your visibility as a player.
-                                    This is the team work aspect to Surface 1.</p>
+                                <p>Work with recruiter to increase your chances of finding
+                                    talented athlete.</p>
                             </div>
                         </div>
                     </div>
-                    <div className="athletes_row3">
-                        <div className="athletes_right">
+                    <div className="players_row3">
+                        <div className="players_right">
                             <div className="growth">
                                 <span>03</span>
                                 <h3>growth</h3>
-                                <p>Resources and tools for you to get better as a student Athelte.
-                                    Access to training classes, tutor sessions, etc </p>
+                                <p>Save your time, recruit proper athlets for your team.</p>
                             </div>
                         </div>
                     </div>
